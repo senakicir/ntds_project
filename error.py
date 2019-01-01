c42a11cd3b1780cabe23e8204238c55bdaf2fca5
Isinsu Katircioglu <katircio@iccluster134.iccluster.epfl.ch>
2019-01-01 15:12:56 +0100

    error.py changed

--- error.py
+++ error.py
@@ -6,5 +6,13 @@ import pdb
 
 from utils import *
 
-def error_func():
-    pass
+def error_func(gt_labels, pred_labels):
+
+    assert gt_labels.shape == pred_labels.shape, "Groundtruth labels should have the same shape as the prediction labels"
+
+    if len(gt_labels.shape) == 2:
+        gt_labels = gt_labels.squeeze(1)
+        pred_labels = pred_labels.squeeze(1)
+        errors = sum(gt_labels != pred_labels)
+        error_rate = (errors/gt_labels.shape[0]) * 100
+    return error_rate
