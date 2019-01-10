3d7c3a16912a5f02ca9fc330056c77af7969993b
Isinsu Katircioglu <isinsu.katircioglu@gmail.com>
2019-01-10 20:49:31 +0100

    gcn dnorm

--- models.py
+++ models.py
@@ -72,22 +72,24 @@ class GraphNeuralNet(torch.nn.Module):
         return F.log_softmax(x, dim=1)
 
 class GCN():
-    def __init__(self, nhid, dropout, adjacency, features, D_norm, labels, cuda=True, lr=0.01, weight_decay=5e-4, epochs=100):
+    def __init__(self, nhid, dropout, adjacency, features, labels, cuda=True, lr=0.01, weight_decay=5e-4, epochs=100):
         self.adjacency = adjacency
         self.features = features
-        self.D_norm = D_norm
         self.labels = labels
         self.nfeat = features.shape[-1]
         self.nhid = nhid
         self.nclass = labels.shape[-1]
         self.epochs = epochs
-        self.gcn = GraphNeuralNet(nfeat, nhid, nclass, dropout)
+        self.gcn = GraphNeuralNet(self.nfeat, nhid, self.nclass, dropout)
         idx_train = range(140)
         idx_val = range(200, 500)
         idx_test = range(500, 1500)
         self.features = torch.FloatTensor(np.array(self.features.todense()))
         self.adjacency = sp.coo_matrix(self.adjacency)
-        self.adjacency = self.D_norm @ (self.adjacency + sp.eye(self.adjacency.shape[0])) @ D_norm
+        self.adjacency = (self.adjacency + sp.eye(self.adjacency.shape[0]))
+        self.D = np.diag(self.adjacency.sum(axis=1))
+        self.D_norm = torch.FloatTensor(np.power(np.linalg.inv(self.D), 0.5))
+        self.adjacency = self.D_norm @ (self.adjacency + sp.eye(self.adjacency.shape[0])) @ self.D_norm
         self.adjacency = sparse_mx_to_torch_sparse_tensor(self.adjacency)
         self.labels = torch.LongTensor(np.where(self.labels)[1])
         self.idx_train = torch.LongTensor(idx_train)
