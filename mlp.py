1638a2ab23d33d26dd0c6e39d5c704831327d8a0
George Adaimi <adaimi@enaccluster003.epfl.ch>
2019-01-18 20:23:51 +0100

    change batch size and epoch

--- mlp.py
+++ mlp.py
@@ -49,7 +49,7 @@ class MyDataset(Dataset):
         return len(self.data)
 
 class MLP():
-    def __init__(self,hidden_size, features, labels,num_epoch,batch_size,num_classes,lr=0.01, save_path="",cuda=True,seed=0):
+    def __init__(self,hidden_size, features, labels,num_epoch,batch_size,num_classes,lr=0.0001, save_path="",cuda=True,seed=0):
         self.features = features
         self.labels = labels.astype(np.int32)
         self.num_classes = num_classes
